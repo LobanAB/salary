521facbc645ad66bed7dd410c027d3c934cceb24
Alex <48862843+LobanAB@users.noreply.github.com>
2021-07-17 17:57:18 +0300

    s10 city in vacancy sj

--- superjob.py
+++ superjob.py
@@ -9,15 +9,17 @@ def main():
     load_dotenv()
     superjob_api_key = os.getenv('SUPERJOB_API_KEY')
     url = 'https://api.superjob.ru/2.0/vacancies/'
-    #payload = {
-    #    'X-Api-App-Id': superjob_api_key
-    #}
+    payload = {
+        'catalogues': 48,
+        'town': 4
+    }
     headers = {'X-Api-App-Id': superjob_api_key}
-    response = requests.get(url, headers=headers)
+    response = requests.get(url, headers=headers, params=payload)
     response.raise_for_status()
     #pprint(response.json()['objects'][0]['profession'])
     for vacancy in response.json()['objects']:
-        print(vacancy['profession'])
+        print(f"{vacancy['profession']}, {vacancy['town']['title']}")
+
 
 if __name__ == '__main__':
     main()
