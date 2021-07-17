c024547ecafcc715a9faf6e3a52483dfd02b2784
Alex <48862843+LobanAB@users.noreply.github.com>
2021-07-17 18:13:38 +0300

    s 11 add func predict_rub_salary_sj()

--- superjob.py
+++ superjob.py
@@ -5,6 +5,21 @@ from pprint import pprint
 from dotenv import load_dotenv
 
 
+def predict_salary(salary_from, salary_to):
+    if salary_from is not None:
+        if salary_to is not None:
+            return (salary_from + salary_to) / 2
+        else:
+            return salary_from * 1.2
+    elif salary_to is not None:
+        return salary_to * 0.8
+
+
+def predict_rub_salary_sj(vacancy):
+    return predict_salary(vacancy['payment_from'] if vacancy['payment_from'] != 0 else None,
+                          vacancy['payment_to'] if vacancy['payment_to'] != 0 else None)
+
+
 def main():
     load_dotenv()
     superjob_api_key = os.getenv('SUPERJOB_API_KEY')
@@ -18,7 +33,9 @@ def main():
     response.raise_for_status()
     #pprint(response.json()['objects'][0]['profession'])
     for vacancy in response.json()['objects']:
+        salary = predict_rub_salary_sj(vacancy)
         print(f"{vacancy['profession']}, {vacancy['town']['title']}")
+        print(salary)
 
 
 if __name__ == '__main__':
